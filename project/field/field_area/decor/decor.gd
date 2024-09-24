bff6bd02290bb6e50e1917f04a821afe58656d3e
Jadi Miller <jadilmiller@gmail.com>
2024-09-24 13:15:19 -0400

    Only allow decor placement with no overlapping

--- project/field/field_area/decor/decor.gd
+++ project/field/field_area/decor/decor.gd
@@ -3,6 +3,9 @@ extends Node2D
 
 @export var decor_type: Decoration
 
+var _overlapping_bodies: Array[Object] = []
+
+var _placeable: bool = false
 var _placed = false
 
 
@@ -17,7 +20,7 @@ func _physics_process(_delta) -> void:
 
 
 func _input(_event):
-	if not _placed and Input.is_action_pressed("place"):
+	if not _placed and _placeable and Input.is_action_pressed("place"):
 		_placed = true
 		global_position = get_global_mouse_position()
 		set_physics_process(false)
@@ -26,4 +29,29 @@ func _input(_event):
 # Using properties from resource to set decor properties
 
 func _set_decor_properties() -> void:
-	$Sprite2D.texture = decor_type.image
+	$Sprite.texture = decor_type.image
+
+
+# Determine if decor is placeable
+
+func _on_placement_detector_body_entered(body: Node2D) -> void:
+	if body != Chicken2D:
+		_overlapping_bodies.append(body)
+		_check_placeable()
+		print(_overlapping_bodies)
+
+
+func _on_placement_detector_body_exited(body: Node2D) -> void:
+	if body != Chicken2D:
+		_overlapping_bodies.erase(body)
+		_check_placeable()
+		print(_overlapping_bodies)
+
+
+func _check_placeable():
+	if _overlapping_bodies.size() > 1:
+		_placeable = false
+		modulate = Color.RED
+	else:
+		_placeable = true
+		modulate = Color.WHITE
